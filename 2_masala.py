23e7cb5b29dec1f4439326d79d348ff9104a0b4f
BahodirZokirov <zokirovbahodir2003@gmail.com>
2024-01-13 23:46:09 +0500

    13.01.2024 keyin qo'shilgan

--- 2_masala.py
+++ 2_masala.py
@@ -1,13 +1,45 @@
-from faker import Faker
-from random import randint
-a = Faker ()
-lt = []
+# from faker import Faker
+# from random import randint
+# a = Faker ()
+# lt = []
+#
+# name = a.name()
+# for i in range (10):
+#     age = randint(1, 100)
+#     lt.append(f"{i}. {name} {age} \n")
+#
+# with open("test1.txt", "w") as file:
+#     for i in lt:
+#         file.write(i)
 
-name = a.name()
-for i in range (10):
-    age = randint(1, 100)
-    lt.append(f"{i}. {name} {age} \n")
 
-with open("test1.txt", "w") as file:
-    for i in lt:
-        file.write(i)
+
+
+
+
+
+# son = int (input ("sonni kiriting:"))
+#
+#
+# k = 0
+# sum_ = 0
+#
+#
+# while sum_ < son:
+#     k += 1
+#     sum_ += k
+# print (k)
+
+
+
+# def f(n):
+#     if n <= 2:
+#         return 1
+#     return 2 * f(n - 1) + f(n - 2)
+#
+# print(f(6))
+
+
+
+
+
